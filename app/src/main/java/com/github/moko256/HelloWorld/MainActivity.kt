bb19f254db929b98a3467c76de3398d2709b1a36
moko256 <koutaro.mo@gmail.com>
2019-08-28 16:56:58 +0900

    Initial commit

---
+++ app/src/main/java/com/github/moko256/HelloWorld/MainActivity.kt
@@ -0,0 +1,10 @@
+﻿package com.github.moko256.HelloWorld
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.widget.TextView(this).apply { setText("Hello, World!") })
+    }
+
+}
