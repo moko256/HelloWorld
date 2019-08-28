10a835dabb4c7e663a1a5441202422a079571920
moko256 <koutaro.mo@gmail.com>
2019-08-28 17:10:15 +0900

    Update MainActivity.kt

--- app/src/main/java/com/github/moko256/HelloWorld/MainActivity.kt
+++ app/src/main/java/com/github/moko256/HelloWorld/MainActivity.kt
@@ -1,10 +1,19 @@
-﻿package com.github.moko256.HelloWorld
+package com.github.moko256.HelloWorld
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(android.widget.TextView(this).apply { setText("Hello, World!") })
+
+        setContentView(
+            TextView(this).apply {
+                setText("Hello, World!")
+            }
+        )
     }
 
 }
